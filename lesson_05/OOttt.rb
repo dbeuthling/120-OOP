f63b799a6210a649c80b6e06a10d7b1b4d5711d3
Dan Beuthling <danbeuthling@gmail.com>
2016-04-06 22:54:45 -0500

    started work on OOttt

---
+++ lesson_05/OOttt.rb
@@ -0,0 +1,99 @@
+require 'pry'
+class Board
+  attr_reader :squares
+
+  def initialize
+    @squares = {}
+    (1..9).each {|key| @squares[key] = Square.new(key.to_s)}
+  end
+
+  def get_square_at(key)
+    @squares[key]
+  end
+
+  def set_square_at(square, marker)
+    @squares[square] = marker
+  end
+end
+
+class Square
+  def initialize(marker)
+    @marker = marker
+  end
+
+  def to_s
+    @marker
+  end
+end
+
+class Player
+  attr_reader :marker
+  def initialize(marker)
+    @marker = marker
+  end
+
+end
+
+class TTTGame
+  attr_reader :board, :human, :computer
+
+  def initialize
+    @board = Board.new
+    @human = Player.new("X")
+    @comptuer = Player.new("O")
+  end
+  def display_welcome_message
+    puts "Welcome to Tic Tac Toe!"
+    puts ""
+  end
+
+  def display_goodbye_message
+    puts "Thank you for playing. Bye now."
+  end
+
+  def display_board
+    puts "     |     |"
+    puts "  #{board.get_square_at(1)}  |  #{board.get_square_at(2)}  |  #{board.get_square_at(3)}"
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "  #{board.get_square_at(4)}  |  #{board.get_square_at(5)}  |  #{board.get_square_at(6)}"
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "  #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |  #{board.get_square_at(9)}"
+    puts "     |     |"
+  end
+
+  def human_moves
+    puts "Choose a square between 1 and 9."
+    square = nil
+    loop do
+      square = gets.chomp.to_i
+      break if (1..9).include?(square)
+      puts "Please pick a valid number."
+    end
+    board.set_square_at(square, human.marker)
+  end
+
+  def computer_moves
+    
+    board.set_square_at(square, computer.marker)
+  end
+
+  def play
+    display_welcome_message
+    loop do
+      display_board
+      human_moves
+      # break if someone_won? || board_full?
+      computer_moves
+      break if someone_won? || board_full?
+    end
+    # display_result
+    display_goodbye_message
+  end
+end
+
+game = TTTGame.new
+game.play
